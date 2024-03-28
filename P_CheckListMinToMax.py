13308678dac84359ad2d9e8d89b5879c193dcb07
TamNhien <canhthien91@gmail.com>
2024-03-28 19:13:27 +0700

    Add project files.

---
+++ P_CheckListMinToMax.py
@@ -0,0 +1,5 @@
+﻿def is_sorted(lst):
+    return all(lst[i] <= lst[i+1] for i in range(len(lst)-1))
+
+lst = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+print(is_sorted(lst))
